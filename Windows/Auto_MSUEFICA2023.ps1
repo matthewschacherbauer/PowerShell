d09f1839e327d54744fff59e3e625a4f5b6db170
Matthew Schacherbauer <48417642+matthewschacherbauer@users.noreply.github.com>
2026-01-08 17:53:32 -0800

    Logic to handle external VM KEK update
    Handle or reprocess changes when VM KEK or entire nvram is changed externally.

--- Windows/Auto_MSUEFICA2023.ps1
+++ Windows/Auto_MSUEFICA2023.ps1
@@ -9,6 +9,11 @@
 
     In testing, this script is assigned without any parameters via a Group Policy scheduled task at startup.
     The script requires up to five reboots to apply all changes and mitigations.
+
+    This script is designed to be safe to run repatedly. If Secure Boot configurations are reset as part of other troubleshooting, the script will detect and
+    reapply mitigations. If no actions are necessary, the script will exit with no changes.
+
+    Tested against Server 2019, 2022, 2025.
     .PARAMETER SkipCAUpdate
     Do not install the updated UEFI CA 2023 certificates.
     .PARAMETER SkipRevocation
@@ -19,15 +24,16 @@
     Do not perform cleanup actions on the AvailableUpdates key.
     .PARAMETER SkipKEKUpdateOnVirtualHardware
     Attempt to detect if the machine is a virtual machine and skip the KEK update if so.
+    Updating the KEK appears to fail on some virtual firmware vendors and may require intervention from outside the Operating System.
     .EXAMPLE
     .\Auto_MSUEFICA2023.ps1
     .EXAMPLE
     .\Auto_MSUEFICA2023.ps1 -SkipKEKUpdateOnVirtualHardware
     .NOTES
     Author:     Matthew Schacherbauer
-    Updated:    2025-12-31
+    Updated:    2026-01-08
 
-    Version:    1.0
+    Version:    1.1
 
     ===============
     This program is free software: you can redistribute it and/or modify
@@ -58,7 +64,8 @@ Param (
     [Switch] $SkipKEKUpdate,
     [Switch] $SkipCleanup,
 
-    [Switch] $SkipKEKUpdateOnVirtualHardware
+    [Switch] $SkipKEKUpdateOnVirtualHardware,
+    [Switch] $WhatIf
 )
 
 #
@@ -89,8 +96,12 @@ Function ApplySecureBootUpdates {
         $Value
     )
 
+    if ( $Value -eq $UEFICA2023State.Registry.AvailableUpdates ) {
+        return
+    }
+
     Write-Verbose "Applying SecureBoot updates with value [0x$('{0:x}' -f $Value) ($($Value))]."
-    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot" -Name AvailableUpdates -Value $Value -Type DWord
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot" -Name AvailableUpdates -Value $Value -Type DWord -WhatIf:$WhatIf
     Start-Sleep -Seconds 2
 
     RunSecureBootUpdateTask -Verbose:$Verbose
@@ -122,6 +133,7 @@ $AvailableUpdatesDefinitions = @{
 $UEFICA2023State = [PSCustomObject] @{
     "Registry" = [PSCustomObject] @{
         "AvailableUpdates" = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot").AvailableUpdates
+        "UEFISecureBootEnabled" = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\State").UEFISecureBootEnabled
         "UEFICA2023Status" = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\Servicing").UEFICA2023Status
         "UEFICA2023Error" = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\Servicing").UEFICA2023Error
         "WindowsUEFICA2023Capable" = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\Servicing").WindowsUEFICA2023Capable
@@ -129,12 +141,29 @@ $UEFICA2023State = [PSCustomObject] @{
         "OEMModelNumber" = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\Servicing\DeviceAttributes").OEMModelNumber
     }
     "DBInstallStatus" = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI db).bytes) -match 'Windows UEFI CA 2023'
-    "ROMInstallStatus" = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI db).bytes) -match 'Microsoft Option ROM UEFI CA 2023'
+    "MSROMInstallStatus" = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI db).bytes)-match 'Microsoft UEFI CA 2023'
+    "OptROMInstallStatus" = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI db).bytes) -match 'Microsoft Option ROM UEFI CA 2023'
     "KEKInstallStatus" = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI kek).bytes) -match 'Microsoft Corporation KEK 2K CA 2023'
     "ThirdPartyInstallStatus" = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI db).bytes) -match 'Microsoft Corporation UEFI CA 2011'
     "DBXRevocationStatus" = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI dbx).bytes) -match 'Microsoft Windows Production PCA 2011'
 }
 $PerformedChanges = $false
+
+# Abort if Secure Boot is not enabled
+if ( $UEFICA2023State.Registry.UEFISecureBootEnabled -ne 1 ) {
+    Write-Warning "Secure Boot is not enabled on the system. Aborting."
+    break
+}
+
+# Check for empty or missing registry keys
+if ( $null -eq $UEFICA2023State.Registry.AvailableUpdates ) {
+    Write-Warning "The AvailableUpdates key is missing. Aborting."
+    break
+}
+if ( $null -eq $UEFICA2023State.Registry.UEFICA2023Status ) {
+    Write-Warning "The UEFICA2023Status key is missing. The system may require updates to be installed. Aborting."
+    break
+}
 
 # Virtual Hardware Detection
 if ( !$SkipKEKUpdate -and $SkipKEKUpdateOnVirtualHardware ) {
@@ -171,10 +200,12 @@ RunSecureBootUpdateTask -Verbose:$Verbose
 Write-Verbose "=========="
 Write-Verbose "Current Values"
 Write-Verbose "AvailableUpdates: [0x$('{0:x}' -f $($UEFICA2023State.Registry.AvailableUpdates)) ($($($UEFICA2023State.Registry.AvailableUpdates)))]"
+Write-Verbose "UEFISecureBootEnabled: [$($UEFICA2023State.Registry.UEFISecureBootEnabled)]"
 Write-Verbose "UEFICA2023Status: [$($UEFICA2023State.Registry.UEFICA2023Status)]"
 Write-Verbose "WindowsUEFICA2023Capable: [$($UEFICA2023State.Registry.WindowsUEFICA2023Capable)]"
 Write-Verbose "DBInstallStatus: [$($UEFICA2023State.DBInstallStatus)]"
-Write-Verbose "ROMInstallStatus: [$($UEFICA2023State.ROMInstallStatus)]"
+Write-Verbose "MSROMInstallStatus: [$($UEFICA2023State.MSROMInstallStatus)]"
+Write-Verbose "OptROMInstallStatus: [$($UEFICA2023State.OptROMInstallStatus)]"
 Write-Verbose "KEKInstallStatus: [$($UEFICA2023State.KEKInstallStatus)]"
 Write-Verbose "ThirdPartyInstallStatus: [$($UEFICA2023State.ThirdPartyInstallStatus)]"
 Write-Verbose "DBXRevocationStatus: [$($UEFICA2023State.DBXRevocationStatus)]"
@@ -189,21 +220,54 @@ Write-Verbose "=========="
 
 if ( !$PerformedChanges -and !$SkipCAUpdate ) {
 
-    # Check if update is applied
-    if ( !$UEFICA2023State.DBInstallStatus -and `
-         $UEFICA2023State.Registry.WindowsUEFICA2023Capable -eq "0" -and `
-         $UEFICA2023State.Registry.UEFICA2023Status -eq "NotStarted" ) {
+    # Check if update is needed
+    if ( $UEFICA2023State.Registry.UEFICA2023Status -eq "Updated" ) {
 
+        Write-Verbose "CA Update already completed. OS reports Updated."
+
+    }
+    elseif ( ( !$UEFICA2023State.DBInstallStatus -or `
+               !$UEFICA2023State.OptROMInstallStatus -or `
+               !$UEFICA2023State.MSROMInstallStatus) -and `
+              $UEFICA2023State.Registry.UEFICA2023Status -eq "NotStarted" ) {
+
+        # Primary update routine.
+        # Install updated certificates if no procedure is in progress.
+        
         Write-Verbose "Determined that the SecureBoot DB certificates need to be updated."
 
         # Determine update value # 0x5940
         Write-Verbose "Calculating update value"
         $NewValue = $UEFICA2023State.Registry.AvailableUpdates
-        if ( !($NewValue -band 64) ) { $NewValue += 64 }          # APPLY_DB_WINUEFICA2023
-        if ( !($NewValue -band 256) ) { $NewValue += 256 }        # APPLY_DB_UEFICA2023_BOOTMGR
-        if ( !($NewValue -band 2048) ) { $NewValue += 2048 }      # APPLY_FW_SVNUPDATE
-        if ( !($NewValue -band 4096) ) { $NewValue += 4096 }      # APPLY_DB_MSUEFICA2023
-        if ( !($NewValue -band 16384) ) { $NewValue += 16384 }    # IFEXIST_DB_PCA2011
+        if ( !$UEFICA2023State.DBInstallStatus -and !($NewValue -band 64) ) { $NewValue += 64 }         # APPLY_DB_WINUEFICA2023
+        if ( !$UEFICA2023State.DBInstallStatus -and !($NewValue -band 256) ) { $NewValue += 256 }       # APPLY_DB_UEFICA2023_BOOTMGR
+        if ( !$UEFICA2023State.OptROMInstallStatus -and !($NewValue -band 2048) ) { $NewValue += 2048 } # APPLY_DB_OPTIONROMUEFI2023
+        if ( !$UEFICA2023State.MSROMInstallStatus -and !($NewValue -band 4096) ) { $NewValue += 4096 }  # APPLY_DB_MSUEFICA2023
+        if ( !($NewValue -band 16384) ) { $NewValue += 16384 }                                          # IFEXIST_DB_PCA2011
+
+        # Update is needed
+        ApplySecureBootUpdates -Value $NewValue -Verbose:$Verbose
+
+        # Don't perform additional steps in same run.
+        $PerformedChanges = $true
+
+    }
+    elseif ( $UEFICA2023State.DBInstallStatus -and `
+             $UEFICA2023State.Registry.UEFICA2023Status -eq "NotStarted" -and `
+             $UEFICA2023State.Registry.WindowsUEFICA2023Capable -in @("0","1") ) {
+
+        # The logic behind WindowsUEFICA2023Capable may vary between OS versions.
+        
+        # Certificates are present but the OS is booting from a boot manager signed by the old certificates.
+        # This can happen when the certificates are updated from outside the operating system.
+        # Two reboots may be required here.
+
+        Write-Verbose "Determined that the SecureBoot DB certificates are present and the boot manager needs to be updated."
+
+        # Determine update value # 0x5940
+        Write-Verbose "Calculating update value"
+        $NewValue = $UEFICA2023State.Registry.AvailableUpdates
+        if ( !($NewValue -band 256) ) { $NewValue += 256 }       # APPLY_DB_UEFICA2023_BOOTMGR
 
         # Update is needed
         ApplySecureBootUpdates -Value $NewValue -Verbose:$Verbose
@@ -214,7 +278,9 @@ if ( !$PerformedChanges -and !$SkipCAUpdate ) {
     }
     elseif ( $UEFICA2023State.DBInstallStatus ) {
 
-        Write-Verbose "CA Update already completed."
+        # The OS doesn't report updated, but all certificates are present.
+
+        Write-Verbose "CA Update already completed. Certificates already installed."
 
     }
     else {
@@ -244,9 +310,16 @@ if ( !$PerformedChanges -and !$SkipRevocation ) {
     # Check if revocation of old certificate is needed
     if ( $UEFICA2023State.DBInstallStatus -and `
          !$UEFICA2023State.DBXRevocationStatus -and `
-         $UEFICA2023State.Registry.WindowsUEFICA2023Capable -eq "2" -and `
-         $UEFICA2023State.Registry.UEFICA2023Status -eq "NotStarted" ) {
+         ( $UEFICA2023State.Registry.UEFICA2023Status -eq "Updated" -or `
+           ( $UEFICA2023State.Registry.WindowsUEFICA2023Capable -eq "2" -and `
+             $UEFICA2023State.Registry.UEFICA2023Status -in @("NotStarted","Updated")
+           )
+         ) 
+       ) {
 
+        # Primary update routine.
+        # Revoke certificates if no procedure is in progress and the updated certificate is installed.
+        
         Write-Verbose "Determined that the SecureBoot DBX needs to be updated."
 
         # Determine update value # 0x280
@@ -292,13 +365,21 @@ else {
 
 if ( !$PerformedChanges -and !$SkipKEKUpdate ) {
 
-    # Check if update is applied
-    if ( $UEFICA2023State.DBInstallStatus -and `
+    # Check if update is needed
+    if ( $UEFICA2023State.Registry.UEFICA2023Status -eq "Updated" ) {
+
+        Write-Verbose "KEK Update already completed. OS reports Updated."
+
+    }
+    elseif ( $UEFICA2023State.DBInstallStatus -and `
          $UEFICA2023State.DBXRevocationStatus -and `
          !$UEFICA2023State.KEKInstallStatus -and `
          $UEFICA2023State.Registry.WindowsUEFICA2023Capable -eq "2" -and `
          $UEFICA2023State.Registry.UEFICA2023Status -eq "NotStarted" ) {
 
+        # Primary update routine.
+        # Install updated certificates if no procedure is in progress.
+        
         Write-Verbose "Determined that the SecureBoot KEK certificates need to be updated."
 
         # Determine update value # 0x4
@@ -313,7 +394,9 @@ if ( !$PerformedChanges -and !$SkipKEKUpdate ) {
         $PerformedChanges = $true
 
     }
-    elseif ( $UEFICA2023State.DBInstallStatus -and $UEFICA2023State.DBXRevocationStatus -and $UEFICA2023State.KEKInstallStatus ) {
+    elseif ( $UEFICA2023State.KEKInstallStatus ) {
+
+        # The OS doesn't report updated, but all certificates are present.
 
         Write-Verbose "KEK Update already completed."
         
@@ -326,10 +409,7 @@ if ( !$PerformedChanges -and !$SkipKEKUpdate ) {
         Write-Verbose "KEKInstallStatus: [$($UEFICA2023State.KEKInstallStatus)]"
         Write-Verbose "WindowsUEFICA2023Capable: [$($UEFICA2023State.Registry.WindowsUEFICA2023Capable)]"
         Write-Verbose "UEFICA2023Status: [$($UEFICA2023State.Registry.UEFICA2023Status)]"
-
-        if ( $UEFICA2023State.Registry.UEFICA2023Status -eq "InProgress" -and $UEFICA2023State.Registry.UEFICA2023Error -eq "2147942419" ) {
-            Write-Verbose "The KEK update is in progress and returned failure code [$($UEFICA2023State.Registry.UEFICA2023Error)]. This possibly means OEM support is not available."
-        }
+        Write-Verbose "UEFICA2023Error: [$($UEFICA2023State.Registry.UEFICA2023Error)]"
 
     }
 
@@ -367,8 +447,9 @@ if ( !$PerformedChanges -and !$SkipCleanup ) {
     # Check if cleanup is needed
     if ( $UEFICA2023State.DBInstallStatus -and `
          $UEFICA2023State.DBXRevocationStatus -and `
-         $UEFICA2023State.Registry.WindowsUEFICA2023Capable -eq "2" -and `
-         $UEFICA2023State.Registry.AvailableUpdates -band 16384 ) {
+         ( $UEFICA2023State.Registry.WindowsUEFICA2023Capable -eq "2" -or `
+           $UEFICA2023State.Registry.UEFICA2023Status -eq "Updated" ) -and `
+         $UEFICA2023State.Registry.AvailableUpdates -eq 16384 ) {
 
         Write-Verbose "Determined that the SecureBoot DB certificates are present and cleanup is needed."
 
@@ -376,14 +457,6 @@ if ( !$PerformedChanges -and !$SkipCleanup ) {
         Write-Verbose "Calculating update value"
         $NewValue = $UEFICA2023State.Registry.AvailableUpdates
         if ( $NewValue -band 16384 ) { $NewValue -= 16384 }     # IFEXIST_DB_PCA2011
-
-        # Determine if KEK cleanup is needed
-        if ( $SkipKEKUpdateOnVirtualHardware -and $NewValue -band 4 ) {
-
-            Write-Verbose "Determined that KEK Update will be cancelled from pending operations."
-            $NewValue = ( $NewValue - 4 ) # 0x4
-
-        }
 
         # Update is needed
         ApplySecureBootUpdates -Value $NewValue -Verbose:$Verbose
@@ -394,7 +467,8 @@ if ( !$PerformedChanges -and !$SkipCleanup ) {
     }
     elseif ( $UEFICA2023State.DBInstallStatus -and `
              $UEFICA2023State.DBXRevocationStatus -and `
-             $UEFICA2023State.Registry.WindowsUEFICA2023Capable -eq "2" -and `
+             ( $UEFICA2023State.Registry.WindowsUEFICA2023Capable -eq "2" -or `
+               $UEFICA2023State.Registry.UEFICA2023Status -eq "Updated" ) -and `
              !($UEFICA2023State.Registry.AvailableUpdates -band 16384) ) {
 
         Write-Verbose "Cleanup already completed."
@@ -450,14 +524,13 @@ return 0
     128     0x0080  P2  DBX   Add Windows Production CA 2011 to Forbidden List (DBX)
     512     0x0200  P2        Apply SVN update to firmware
 
-    4       0x0004  P3  DB    Apply Microsoft Corporation KEK 2K CA 2023 (Seems to fail on VM?)
+    4       0x0004  P3  DB    Apply Microsoft Corporation KEK 2K CA 2023
 
     P1,P3 covers SecureBoot update
     P2 covers BlackLotus
 #>
 
 <#
-
     CERTIFICATE RENEWALS
     STORE   OLD                                         =>  NEW
     KEK     Microsoft Corporation KEK CA 2011               Microsoft Corporation KEK 2K CA 2023
@@ -465,6 +538,16 @@ return 0
     DB      Microsoft Corporation UEFI CA 2011 (*)          Microsoft UEFI CA 2023
     DB      Microsoft Corporation UEFI CA 2011 (*)          Microsoft Option ROM UEFI CA 2023
             (*) NEW is only added if OLD certificate is present
+#>
 
+<#
+    VIRTUAL MACHINE NOTES
+    Per Broadcom, VMware VMs created prior to 8.0.2 do not contain the 2023 KEK certificate and must be
+    powered off and have their NVRAM file deleted to regenerate with the updated certificate.
+    Presumably, the VMX version needs to be upgraded to at least HWv21.
+    https://knowledge.broadcom.com/external/article/421593/missing-microsoft-corporation-kek-ca-202.html
+
+    ESX 9.0 seems to have an advanced option to do a reset.
+    https://knowledge.broadcom.com/external/article?articleNumber=377306
 #>
 
